e873881b8150cb3a944624c608998e621bc002b5
Nikhil Singh <singh789@gmail.com>
2022-12-02 13:18:00 +0530

    HELLO WORLD

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 var React= require('react');
 var ReactDOM =  require("react-dom");
 
-ReactDOM.render(<h1>Hello World !</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World </h1>, document.getElementById('root'));
