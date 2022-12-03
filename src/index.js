3f385467936d7dfcc06042348d6e63d7166910ee
Nikhil Singh <singh789@gmail.com>
2022-12-03 10:48:50 +0530

    pure Javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-var React= require('react');
-var ReactDOM =  require("react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World !</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello!</h1>, document.getElementById('root'));
+
+var h1= document.createElement("h1");
+h1.innerHTML="Hi Nikhil";
+document.getElementById("root").appendChild(h1);
